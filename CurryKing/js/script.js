006374a0d7f9afa8f7298a940638f38e27c1360a
MinhLeTrinh <33784560+MinhLeTrinh@users.noreply.github.com>
2020-01-21 11:19:32 +0700

    Add files via upload

---
+++ CurryKing/js/script.js
@@ -0,0 +1,51 @@
+
+(function($) {
+    "use strict"; 
+
+    
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 80)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+	
+	
+	$(function()
+		{
+			$('[rel="lightbox"]').lightbox();
+	});
+	
+  
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+	
+	$(".copyright").after(atob("PGRpdiBjbGFzcz0nY29sLW1kLTYnPjxkaXYgY2xhc3M9J3JpZ2h0LXRleHQnPkRlc2lnbiAmYW1wOyBEZXZlbG9wZWQgYnk6IDxhIGhyZWY9J2h0dHBzOi8vYm9vdHN0cmFwd2VidGVtcGxhdGVzLmNvbS8nIHRhcmdldD0nX2JsYW5rJz5Cb290c3RyYXAgV2Vic2l0ZSBUZW1wbGF0ZXM8L2E+PC9kaXY+PC9kaXY="));
+
+
+})(jQuery); 
